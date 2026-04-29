fce54cfe8c29b303cf1827d927377ebff428c20e
IbrohimZabibullayev <iadhamoff990@gmail.com>
2026-04-29 14:03:43 +0500

    Fix CORS preflight for Super Admin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -34,10 +34,12 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    origin: function (origin, callback) {
+      // Allow any origin
+      callback(null, true);
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'x-tenant-id', 'x-super-admin-key'],
   });
 
   const port = parseInt(process.env.PORT || '4000', 10);
