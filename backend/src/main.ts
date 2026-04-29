f968b813bccad0e4f5ce9d74e09c9d1e5a2bb39c
IbrohimZabibullayev <iadhamoff990@gmail.com>
2026-04-29 14:56:50 +0500

    Fix crypto crash and cleanup main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,3 @@
-import { webcrypto } from 'crypto';
-(globalThis as any).crypto = webcrypto;
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
